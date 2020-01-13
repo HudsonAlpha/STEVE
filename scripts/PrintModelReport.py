f7d5a28d4fe9a553e81cd95a1589ffcb605b6305
holtjma <jholt@hudsonalpha.org>
2020-01-13 15:01:57 -0600

    adding the clinical selection output

--- scripts/PrintModelReport.py
+++ scripts/PrintModelReport.py
@@ -9,10 +9,13 @@ import os
 
 from ExtractFeatures import GT_TRANSLATE, VAR_TRANSLATE
 
-def printAllStats(modelDir, rocDir):
+def printAllStats(modelDir, rocDir, minRecall, targetRecall):
     '''
     This will print out our model statistics in an ingestible format
     @param modelDir - the model directory 
+    @param rocDir - a directory for output ROC curve images
+    @param minRecall - the minimum recall values allowed
+    @param targetRecall - the target recall we want the models to achieve
     '''
     #read in the stats
     jsonFN = '%s/stats.json' % (modelDir, )
@@ -36,6 +39,8 @@ def printAllStats(modelDir, rocDir):
 
         if rocDir != None:
             createRocImage(reformKey, rocs[k], rocDir)
+    
+    printClinicalModels(stats, minRecall, targetRecall)
 
 def printModelStats(modelType, stats):
     '''
@@ -98,11 +103,82 @@ def createRocImage(modelType, rocStats, rocDir):
     plt.ylabel('TPR / recall')
     plt.xlim([0.0000, 1.0000])
     plt.ylim([0.9900, 1.0000])
+    #plt.ylim([0.0000, 1.0000])
     plt.axhline(0.9950, color='red', linestyle='--')
     plt.legend()
     plt.grid()
     plt.savefig(outFN)
     plt.close()
+
+def printClinicalModels(allStats, acceptedRecall, targetRecall):
+    header = [
+        'variant_type', 'best_model', 'model_target',
+        'CV_recall', 'final_recall', 'CV_FPR', 'final_FPR'
+    ]
+    print(*header, sep='\t')
+    for k in sorted(allStats.keys()):
+        stats = allStats[k]
+        reformKey = VAR_TRANSLATE[int(k.split('_')[0])]+'_'+GT_TRANSLATE[int(k.split('_')[1])]
+
+        bestModelTargetRecall = None
+        bestModelName = None
+        bestHM = 0.0
+        for mn in stats.keys():
+            for tr in stats[mn]['ALL_SUMMARY']:
+                #CM = confusion matrix
+                modelCM = np.array(stats[mn]['ALL_SUMMARY'][tr]['TEST_CM'][0])
+                if (np.sum(modelCM[:, 1]) == 0.0 or np.sum(modelCM[1, :]) == 0):
+                    modelHM = 0.0
+                else:
+                    modelRecall = modelCM[1, 1] / (modelCM[1, 0] + modelCM[1, 1])
+                    trainTPR = np.array(stats[mn]['LEAVEONEOUT_SUMMARY'][tr]['TEST_TPR'])
+                    trainAvg = np.mean(trainTPR)
+                    trainStd = np.std(trainTPR)
+
+                    #if the average training low end is too low OR the final model is outside the training bounds
+                    # THEN we will not use the model
+                    twoSDBottom = trainAvg - 2*trainStd
+                    if (twoSDBottom < acceptedRecall or
+                        modelRecall < twoSDBottom):
+                        modelHM = 0.0
+                    else:
+                        #in clinical, best is harmonic mean of our adjusted recall and our TNR
+                        modelTNR = modelCM[0, 0] / (modelCM[0, 0] + modelCM[0, 1])
+                        #adjRecall = (modelRecall*100 - 99)
+                        #adjRecall = modelRecall
+                        adjRecall = (modelRecall - acceptedRecall) / (float(targetRecall) - acceptedRecall)
+                        if adjRecall > 1.0:
+                            adjRecall = 1.0
+                        modelHM = 2 * adjRecall * modelTNR / (adjRecall+modelTNR)
+                        
+                if modelHM > bestHM:
+                    bestModelTargetRecall = tr
+                    bestModelName = mn
+                    bestHM = modelHM
+
+                    bestTPRAvg = trainAvg
+                    bestTPRStd = trainStd
+                    bestTPR = modelRecall
+
+                    bestFPRAvg = np.mean(stats[mn]['LEAVEONEOUT_SUMMARY'][tr]['TEST_FPR'])
+                    bestFPRStd = np.std(stats[mn]['LEAVEONEOUT_SUMMARY'][tr]['TEST_FPR'])
+                    bestFPR = stats[mn]['ALL_SUMMARY'][tr]['TEST_FPR'][0]
+        
+        if bestModelName == None:
+            #this is the unfortunate event that NO model passes 
+            print(reformKey, 'None', 'None', '--', '--', '--', '--', sep='\t')
+        else:
+            modelTargetRecall = bestModelTargetRecall
+            evalList = [bestModelName]
+            rowVals = [
+                reformKey, bestModelName, bestModelTargetRecall,
+                '%0.4f+-%0.4f' % (bestTPRAvg, bestTPRStd), 
+                '%0.4f' % bestTPR,
+                '%0.4f+-%0.4f' % (bestFPRAvg, bestFPRStd), 
+                '%0.4f' % bestFPR
+            ]
+            print(*rowVals, sep='\t')
+    
 
 if __name__ == "__main__":
     #first set up the arg parser
@@ -112,6 +188,8 @@ if __name__ == "__main__":
     #optional arguments with default
     #p.add_argument('-r', '--recall', dest='recall', default='0.99', help='the target recall value from training (default: 0.99)')
     p.add_argument('-r', '--roc-dir', dest='roc_dir', default=None, help='a directory to store ROC images (default: None)')
+    p.add_argument('-m', '--min-recall', dest='min_recall', default=0.99, type=float, help='the minimum recall for clinical applications (default: 0.990)')
+    p.add_argument('-t', '--target-recall', dest='target_recall', default=0.995, type=float, help='the target recall for clinical applications (default: 0.995)')
 
     #required main arguments
     p.add_argument('model_directory', type=str, help='directory with models and model stats')
@@ -119,4 +197,4 @@ if __name__ == "__main__":
     #parse the arguments
     args = p.parse_args()
 
-    printAllStats(args.model_directory, args.roc_dir)
+    printAllStats(args.model_directory, args.roc_dir, args.min_recall, args.target_recall)
